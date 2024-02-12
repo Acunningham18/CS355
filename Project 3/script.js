b68461fafcbf516a26ef16fb5118d358deef0513
Alexia Cunningham <acunningham@caldwell.edu>
2024-02-12 18:25:10 -0500

    new cahnges

--- Project 3/script.js
+++ Project 3/script.js
@@ -39,3 +39,27 @@ showImagesBtn.addEventListener('click', () => {
         })
         .catch(error => console.error('Error fetching images:', error));
 });
+
+// Apply dark mode on page load if saved in localStorage
+
+const $ = document.querySelector.bind(document);
+
+function toggleDark() {
+    if ($('body').hasAttribute('dark-mode')) {
+        localStorage.setItem('mode', "light");
+        $('body').removeAttribute('dark-mode');
+    } else {
+        localStorage.setItem('mode', 'dark');
+        $('body').setAttribute('dark-mode', true);
+    }
+}
+
+$('#darkBtn').addEventListener('click', toggleDark);
+
+function main() {
+    if (localStorage.getItem('mode') === 'dark') {
+        $('body').setAttribute('dark-mode', true);
+    }
+}
+
+main(); // Call the main function on page load
